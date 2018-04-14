bb39b38476bd1cf43bb9af56d319380d6bd8e913
djcopley <djcopley@users.noreply.github.com>
2018-04-13 21:28:15 -0700

    Added argument parsing

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,9 +1,10 @@
 """
 Code by Daniel Copley
 Source @ GitHub.com/djcopley
+Version 0.1-beta
 
 The goal of version 2.0 is to reply directly to threads where you are mentioned.
-TODO - Add logging and argparse; Reply directly to tweets
+TODO - Setup logging; Reply directly to tweets
 """
 import twitter
 import re
@@ -21,6 +22,18 @@ api = twitter.Api(
 
 cleverbot = CleverWrap(api_key=CleverBot.API_KEY)
 
+# TODO Setup logger
+logger = logging.Logger('')
+
+# Argument Parsing
+parser = argparse.ArgumentParser(description='A bot that automatically responds to tweets.',
+                                 prog='Twitter Bot')
+parser.add_argument('user_handle', help='your twitter @handle', type=str)
+parser.add_argument('-m', '--mute', help='disables console output of tweets', action='store_true', default=False)
+parser.add_argument('-l', '--log', help='change logging level', type=int, choices=[0, 1, 2])
+
+arguments = parser.parse_args()
+
 
 def send_tweet(handle, text):
     try:
@@ -31,7 +44,7 @@ def send_tweet(handle, text):
 
 
 def strip_user_handles(text):
-    """Function strips user handles from incoming tweet"""
+    """ Function strips user handles from incoming tweet """
     pattern = r'\B@\w+ *'
     return re.sub(pattern, '', text)
 
@@ -39,13 +52,14 @@ def strip_user_handles(text):
 if __name__ == '__main__':
     try:
         # Be sure to replace @djrcopley with YOUR twitter handle
-        for tweet in api.GetStreamFilter(track=['@djrcopley']):
+        for tweet in api.GetStreamFilter(track=[arguments.user_handle]):
             response = cleverbot.say(strip_user_handles(tweet['text']))
             send_tweet(tweet['user']['screen_name'], response)
 
-            print('Tweet from @{}'.format(tweet['user']['screen_name']))
-            print(tweet['text'])
-            print('Reply: {}'.format(response))
+            if not arguments.mute:
+                print('Tweet from @{}'.format(tweet['user']['screen_name']))
+                print(tweet['text'])
+                print('Reply: {}'.format(response))
 
     except KeyboardInterrupt:
         print('\nQuitting')
